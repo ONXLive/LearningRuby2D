921eb3d170b07fc9df2112cbdd87e58d970ccda0
onyxcode <talk@onyxcode.space>
2020-06-09 21:08:35 -0400

    a

---
+++ pimp_my_square.rb
@@ -0,0 +1,15 @@
+require 'ruby2d'
+
+# Create a new, green square
+Square.new(color: 'white')
+
+# Create variable
+s = Square.new
+
+# Use hexadecimal values to set a color
+s.color = '#ff0000'
+
+# Use red, green, blue, alpha values to set the color
+s.color = [1, 0.5, 0.2, 1]
+
+# UNFINISHED FILE
